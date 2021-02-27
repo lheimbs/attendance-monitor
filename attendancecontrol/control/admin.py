4633a38a7d5bc73ed34f291cc9e7f3d568b7cf21
Lennart Heimbs <l.heimbs@gmail.com>
2021-02-27 22:47:36 +0100

    Add models to admin

--- attendancecontrol/control/admin.py
+++ attendancecontrol/control/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User
+from .models import User, Student, Teacher, Course, WeekDay
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username']
 
 
 # Register your models here.
 admin.site.register(User, CustomUserAdmin)
+admin.site.register((Student, Teacher, Course, WeekDay))
