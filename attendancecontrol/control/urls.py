89c0d23273c4526cebc339b4af0d984a6b37ef57
Lennart Heimbs <l.heimbs@gmail.com>
2021-02-27 22:49:52 +0100

    Add teacher urls; move courses view

--- attendancecontrol/control/urls.py
+++ attendancecontrol/control/urls.py
@@ -6,15 +6,17 @@ urlpatterns = [
     path('', home.home, name='home'),
     path('students/', include(([
         path('', students.StudentCoursesList.as_view(), name='courses'),
-        path('<int:pk>', students.StudentCourseDetail.as_view(), name='course-detail'),
+        path('course/<int:pk>', students.StudentCourseDetail.as_view(), name='detail'),
         # path('interests/', students.StudentInterestsView.as_view(), name='student_interests'),
         # path('taken/', students.TakenQuizListView.as_view(), name='taken_quiz_list'),
         # path('quiz/<int:pk>/', students.take_quiz, name='take_quiz'),
     ], 'control'), namespace='students')),
-    path('teachers/', include(([
+    path('teacher/', include(([
         path('', teachers.TeacherCoursesList.as_view(), name='courses'),
-        # path('interests/', students.StudentInterestsView.as_view(), name='student_interests'),
-        # path('taken/', students.TakenQuizListView.as_view(), name='taken_quiz_list'),
+        path('course/<int:pk>', teachers.TeacherCourseDetail.as_view(), name='detail'),
+        path('new/', teachers.TeacherCreateCourse.as_view(), name='add'),
+        path('edit/', teachers.TeacherCoursesList.as_view(), name='edit'),
+        path('delete/', teachers.TeacherCoursesList.as_view(), name='delete'),
         # path('quiz/<int:pk>/', students.take_quiz, name='take_quiz'),
-    ], 'control'), namespace='teachers')),
+    ], 'control'), namespace='teacher')),
 ]
