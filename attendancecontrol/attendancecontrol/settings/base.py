a1198165168cd04f0c80d9517c4268e170563ce8
Lennart Heimbs <l.heimbs@gmail.com>
2021-03-05 23:43:36 +0100

    change language_code to de-de

--- attendancecontrol/attendancecontrol/settings/base.py
+++ attendancecontrol/attendancecontrol/settings/base.py
@@ -121,7 +121,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
 TIME_ZONE = 'Europe/Berlin'
 
