15a69c7d2c63e49bccd0e4d63400fb8b35c4bd7b
Lennart Heimbs <l.heimbs@gmail.com>
2021-03-07 23:28:04 +0100

    Add migration rules for probes db

--- attendancecontrol/control/dbrouters.py
+++ attendancecontrol/control/dbrouters.py
@@ -1,12 +1,27 @@
 from .models.probes import ProbeRequests
 
-class ProbeRequestsRouter(object):
+class ProbeRequestsRouter(object):      # pragma: no cover
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the auth and contenttypes apps only appear in the
+        'auth_db' database.
+        """
+        if db == "probes":
+            if model_name == ProbeRequests._meta.model_name:
+                return True
+            else:
+                return False
+        elif model_name == ProbeRequests._meta.model_name:
+            return False
+            # return db == 'auth_db'
+        return None
 
     def db_for_read(self, model, **hints):
         """ reading ProbeRequests from probes DB """
         if model == ProbeRequests:
             return 'probes'
-        return None
+        return 'default'
 
     def db_for_write(self, model, **hints):
         """ writing SomeModel to otherdb """
