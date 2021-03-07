11e974c0b0a892d3273f61df113f49c800bf4194
Lennart Heimbs <l.heimbs@gmail.com>
2021-03-07 23:29:18 +0100

    Add basic dbrouter tests to ensure working probes table

---
+++ attendancecontrol/control/tests/test_dbrouters.py
@@ -0,0 +1,26 @@
+from django.test import TransactionTestCase, override_settings
+
+from ..models.probes import ProbeRequests
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+    'probes': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+
+@override_settings(DATABASES=DATABASES)
+class ProbeRequestsRouterTest(TransactionTestCase):
+    databases = {'default', 'probes'}
+
+    def test_dbrouter_uses_probes_as_db(self):
+        ProbeRequests.objects.last()
+        self.assertIs(True, False)
+
+    def test_dbrouter_raises_exception_when_try_to_create_probe_request(self):
+        self.assertRaises(BaseException, ProbeRequests.objects.create)
