8f993bf18b6ec0bdaf4ed48297c4635898b1676d
Lennart Heimbs <l.heimbs@gmail.com>
2021-04-08 12:25:55 +0200

    Update channels to use sync db connection

--- attendancecontrol/control/consumers.py
+++ attendancecontrol/control/consumers.py
@@ -1,21 +1,33 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
 
 from .views import probes
+
 
 class StudentCourseProbesConsumer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope["user"]
         if self.user.is_student:
             self.accept()
+        else:
+            self.close()
 
     def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
+    @database_sync_to_async
+    def get_course(self, course_pk):
+        return self.user.student.courses.get(pk=course_pk)
+    
+    @database_sync_to_async
+    def get_students_probes_fig(self, course):
+        return probes.get_students_probe_records(self.user.student, course)
+
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        course = self.user.student.courses.get(pk=text_data_json.get('course'))
-        fig = probes.get_students_probe_records(self.user.student, course)
+        course = await self.get_course(text_data_json.get('course'))
+        fig = await self.get_students_probes_fig(course)
 
         self.send(text_data=json.dumps({
             'graph': fig.to_html(full_html=False)
--- attendancecontrol/control/views/home.py
+++ attendancecontrol/control/views/home.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 # Create your views here.
 def home(request):
@@ -7,4 +10,11 @@ def home(request):
         return redirect('teacher:courses')
     elif request.user.is_authenticated and request.user.is_student:
         return redirect('student:courses')
+    elif request.user.is_authenticated:
+        messages.add_message(request, messages.ERROR, (
+            "You are neither a student nor a teacher. "
+            "Please either contact the administrator to assign your account a role or "
+            "login with a different account.")
+        )
+        logout(request)
     return render(request, 'control/home.html')
