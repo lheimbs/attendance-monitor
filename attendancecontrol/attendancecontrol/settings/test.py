4a40f95aba8ae3bb81659d69be7fa354d945f0f9
Lennart Heimbs <l.heimbs@gmail.com>
2021-06-13 23:14:36 +0200

    Integrate extra probes db into main db

--- attendancecontrol/attendancecontrol/settings/test.py
+++ attendancecontrol/attendancecontrol/settings/test.py
@@ -2,33 +2,33 @@ import os
 from .base import *     # noqa: F403
 from django.test.runner import DiscoverRunner
 
-class UnManagedModelTestRunner(DiscoverRunner):
+# class UnManagedModelTestRunner(DiscoverRunner):
 
-    def setup_test_environment(self, *args, **kwargs):
-        from django.apps import apps
-        self.unmanaged_models = [m for m in apps.get_models() if not m._meta.managed]
-        for m in self.unmanaged_models:
-            m._meta.managed = True
-        super(UnManagedModelTestRunner, self).setup_test_environment(*args, **kwargs)
+#     def setup_test_environment(self, *args, **kwargs):
+#         from django.apps import apps
+#         self.unmanaged_models = [m for m in apps.get_models() if not m._meta.managed]
+#         for m in self.unmanaged_models:
+#             m._meta.managed = True
+#         super(UnManagedModelTestRunner, self).setup_test_environment(*args, **kwargs)
 
-    def teardown_test_environment(self, *args, **kwargs):
-        super(UnManagedModelTestRunner, self).teardown_test_environment(*args, **kwargs)
-        # reset unmanaged models
-        for m in self.unmanaged_models:
-            m._meta.managed = False
+#     def teardown_test_environment(self, *args, **kwargs):
+#         super(UnManagedModelTestRunner, self).teardown_test_environment(*args, **kwargs)
+#         # reset unmanaged models
+#         for m in self.unmanaged_models:
+#             m._meta.managed = False
 
 
-TEST_RUNNER = 'attendancecontrol.settings.test.UnManagedModelTestRunner'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'probes': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'probes.sqlite3',
-        'TEST': {
-            'MIGRATE': False,
-        }
-    },
-}
+# TEST_RUNNER = 'attendancecontrol.settings.test.UnManagedModelTestRunner'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     'probes': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'probes.sqlite3',
+#         'TEST': {
+#             'MIGRATE': False,
+#         }
+#     },
+# }
